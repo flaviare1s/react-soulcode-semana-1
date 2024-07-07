5ac1f770a9a000ce5aab31bbe0bf8d8f1226e63c
flaviare1s <flaviarster@gmail.com>
2024-07-07 17:44:41 -0300

    Corrigi componente de produto

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -4,9 +4,9 @@ const Produto = ({nome, precoUnitario, desconto}) => {
   return (
     <article className='produto'>
       <h2>{nome}</h2>
-      <p className='preco-unitario'>R$ {precoUnitario}</p>
-      <span className='preco-desconto'>R$ {(precoUnitario - (precoUnitario * (desconto / 100))).toFixed()}</span>
-      <span className='desconto'>{desconto}% OFF</span>
+      {desconto > 0 ? <p className='preco-cortado'>R$ {precoUnitario}</p> : <p className='preco-unitario'>R$ {precoUnitario}</p>}
+      {desconto > 0 && <span className='preco-desconto'>R$ {(precoUnitario - (precoUnitario * (desconto / 100))).toFixed()}</span>}
+      {desconto > 0 && <span className='desconto'>{desconto}% OFF</span>}
     </article>
   )
 }
